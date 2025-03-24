b7dea2ca2e35b16f19cc5e54a0fab15cca26bf06
Nazrina <nazrinakhan88@gmail.com>
2025-03-23 23:06:55 -0400

    frontend

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
  git pull
- ansible-playbook -i  $1.dev.naifah.online, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+ #ansible-playbook -i  $1.dev.naifah.online, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
+
+ ansible-playbook -i  172.31.21.157, -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321 -e role_name=$1 main.yml
